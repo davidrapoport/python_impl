6703a5bb9f326c81d3ae4822cca544a6bc0a5fab
davidrapoport <drapoport847@gmail.com>
2015-05-16 23:54:01 -0400

    Finished product, damn is the solution online more elegant

--- itertools2.py
+++ itertools2.py
@@ -1,4 +1,4 @@
-
+import copy
 
 def count(start, step=1):
 	while 1:
@@ -22,7 +22,122 @@ def repeat(elem, n=None):
 			yield elem
 
 def chain(*its):
-	# for it in its:
-	# 	for elem in it:
-	# 		yield elem
 	return (elem for it in its for elem in it)
+
+def compress(data, selectors):
+	for d,s in zip(data,selectors):
+		if s:
+			yield d
+
+def dropwhile(pred, seq):
+	flag = False
+	for i in seq:
+		flag = flag or (not pred(i))
+		if flag:
+			yield i
+
+def groupby(iterable, keyfunc=None):
+	keys = []
+	groups = []
+	if keyfunc is None or not hasattr(keyfunc,'__call__'):
+		keyfunc = lambda x: x
+	if not len(iterable):
+		return ([],[])
+	key = keyfunc(iterable[0])
+	l = []
+	l.append(iterable[0])
+	keys.append(key)
+	for i in iterable[1:]:
+		if key != keyfunc(i):
+			key =keyfunc(i)
+			groups.append(l)
+			l = []
+			l.append(i)
+			keys.append(key)
+		else:
+			l.append(i)
+	if l:
+		groups.append(l)
+	return zip(keys,groups)
+
+def ifilter(seq, pred=None):
+	if pred is None:
+		pred = lambda x: x
+	return (s for s in seq if pred(s))
+
+def ifilterfalse(seq, pred=None):
+	if pred is None:
+		pred = lambda x: x
+	return (s for s in seq if not pred(s))
+
+def islice(seq,start=0,stop=None,step=1):
+	while 1:
+		if stop and start>stop:
+			return
+		try:
+			l = seq[start]
+			start += step
+			yield l
+		except:
+			return
+def imap(func, *args):
+	for values in zip(args):
+		if func:
+			yield func(*values)
+		else:
+			yield tuple(*values) #is the call to tuple necessary?
+
+def starmap(func, seq):
+	return (func(*i) for i in seq)
+
+def tee(it, n):
+	pass #Dont get it right now
+
+def takewhile(pred, seq):
+	if pred is None:
+		pred = bool
+	if not len(seq):
+		return
+	i = 0
+	while i<len(seq) and pred(seq[i]):
+		yield seq[i]
+		i += 1
+
+def izip(*its):
+	iterables = map(iter, its)
+	while 1:
+		try:
+			yield tuple(map(next,iterables))
+		except:
+			return
+
+def izip_longest(*its, **kwargs):
+	fillvalue = kwargs.get("fillvalue")
+	lists=map(list,its)
+	longest = reduce(lambda acc,x: max(acc,len(x)),lists, 0)
+	iterables = map(iter,lists)
+	for i in range(longest):
+		yield tuple([next(x,fillvalue) for x in iterables])
+		#Alternate version using any() to see if any of them aint fillvalue? instead of getting longest?
+
+def product(*args, **kwargs):
+	def inner(*seqs, **kwargs):
+		repeat = kwargs.get("repeat")
+		if repeat:
+			seqs = seqs*repeat
+		if not len(seqs):
+			yield []
+		elif len(seqs) == 1:
+			for i in seqs[0]:
+				yield [i]
+		else:
+			temp = list(inner(*seqs[1:])) # Has to be a list otherwise it gets exhausted
+			for i in seqs[0]:
+				for lst in temp:
+					l = [i]+copy.deepcopy(lst)
+					yield l
+
+	for i in inner(*args,**kwargs):
+		yield tuple(i)
+
+
--- itertoolstest.py
+++ itertoolstest.py
@@ -1,4 +1,45 @@
-from itertools2 import *
+import itertools2 as it
+import itertools as it_orig
+import unittest
 
-def chain_test():
-	assert list(chain(range(3),range(5))) == [0,1,2,0,1,2,3,4]
+class TestItertools(unittest.TestCase):
+
+	def test_chain(self):
+		chain_list = list(it.chain(range(3),range(5)))
+		self.assertEqual([0,1,2,0,1,2,3,4],chain_list)
+
+	def test_count(self):
+		l = []
+		gen = it.count(0,2)
+		for i in gen:
+			l.append(i)
+			if i>=10:
+				break
+		self.assertEqual(len(l),6)
+		self.assertEqual(l,[0,2,4,6,8,10])
+
+	def test_cycle(self):
+		gen = it.cycle(range(5))
+		cnt = 0
+		l = []
+		for i in gen:
+			if cnt>=10:
+				break
+			l.append(i)
+			cnt +=1
+		self.assertEqual(len(l),10)
+		s = set([0,1,2,3,4])
+		self.assertEqual(s|set(l),s&set(l))
+
+	def test_repeate(self):
+		l = list(it.repeat('a',12))
+		k = list(it_orig.repeat('a',12))
+		self.assertEqual(l,k)
+
+	def test_groupby(self):
+		self.assertEqual(it.groupby([1,1,1,1,2,2,2,3,3,1]), [(key,list(group)) for (key,group) in it_orig.groupby([1,1,1,1,2,2,2,3,3,1])])
+		l = lambda x: x%2
+		self.assertEqual(it.groupby([1,2,3,5,7,9,4,4,4,8,7],l),[(key,list(group)) for (key,group) in it_orig.groupby([1,2,3,5,7,9,4,4,4,8,7],l)])
+
+if __name__=='__main__':
+	unittest.main()
